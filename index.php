30d4c55ab0cf2a878143d1338988ea64ca0a1dc3
Azerlund6214 <67274297+Azerlund6214@users.noreply.github.com>
2020-08-01 16:24:19 +0300

    Upd

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
     //TODO: Написать обработку ? в uri
     // В логи пишутся только успешные переходы
 
+    $debug_mode = true; // Выводить все переменные и не делать реальный редирект в конце
+    #$debug_mode = false;
+
+
+    $logging_active = true; # Писать ли логи в бд
+
+
     $db_host = '127.0.0.1';
     $db_user = 'root';
     $db_pass = 'root';
@@ -40,16 +47,45 @@
     $sql = "SELECT * FROM directions WHERE uri='$request_uri'";
     $result = $DBC -> Query($sql , "assoc");
 
-
     if ( count( $result) === 0 )
         header("Location: ".SF::Get_This_Server_Domain()."/uri_not_found" );
 
 
-    SF::PRINTER($result);
+    if( $debug_mode ) SF::PRINTER($result);
+    /*  [uri] => /uri_not_found
+        [destination] => https://yandex.ru/
+        [delay_ms] => 5
+        [description] => Если не нашли uri в этой таблице  */
 
-    //usleep();
 
 
+
+    if( $debug_mode ) echo "<br>Спим ".$result['delay_ms']."мс" ;
+
+    usleep( $result['delay_ms'] );
+
+
+
+
+    if( $logging_active )
+    {
+        $sql = "INSERT INTO logs (uri, destination )
+                VALUES(  '".$request_uri."' , '".$result['destination'] ."' )";
+
+        //echo "<br>".$sql; exit();
+
+
+        if ($debug_mode) echo "<br>Пишем в лог => $sql";
+        $DBC->Exec($sql);
+    }
+
+
+
+    if( $debug_mode ) exit( "<br>Сейчас был бы переход в => ".$result['destination']."<hr>Exit" ) ;
+
+
+
+    header("Location: ".$result['destination'] );
 
 
 
